fb85deae16ba74de054d355e95c021d5e33b2b96
Luis Maestre <luismaestre@luiss-mbp.attlocal.net>
2017-08-27 22:24:19 -0500

    new

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,62 @@
+
+$(document).ready(function(){
+  $('input[name=clowns]').change(function(){
+    if($('input[name=clowns]:checked').val() === "CORRECT") {
+      alert('You have selected the correct answer!');
+    } else {
+      alert('You have selected the wrong answer!');
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('input[name=gym]').change(function(){
+    if($('input[name=gym]:checked').val() === "CORRECT") {
+      alert('You have selected the correct answer!');
+    } else {
+      alert('You have selected the wrong answer!');
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('input[name=food]').change(function(){
+    if($('input[name=food]:checked').val() === "CORRECT") {
+      alert('You have selected the correct answer!');
+    } else {
+      alert('You have selected the wrong answer!');
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('input[name=brain]').change(function(){
+    if($('input[name=brain]:checked').val() === "CORRECT") {
+      alert('You have selected the correct answer!');
+    } else {
+      alert('You have selected the wrong answer!');
+    }
+  });
+});
+$(document).ready(function(){
+  $('input[name=mem]').change(function(){
+    if($('input[name=mem]:checked').val() === "CORRECT") {
+      alert('You have selected the correct answer!');
+    } else {
+      alert('You have selected the wrong answer!');
+    }
+  });
+});
+$(document).ready(function(){
+  $('input[name=think]').change(function(){
+    if($('input[name=think]:checked').val() === "CORRECT") {
+      alert('You have selected the correct answer!');
+    } else {
+      alert('You have selected the wrong answer!');
+    }
+  });
+});
+
 document.getElementById('timer').innerHTML =
   05 + ":" + 00;
 startTimer();
@@ -8,7 +67,7 @@ function startTimer() {
   var m = timeArray[0];
   var s = checkSecond((timeArray[1] - 1));
   if(s==59){m=m-1}
-  //if(m<0){alert('timer completed')}
+  if(m<0){alert('timer completed')}
   
   document.getElementById('timer').innerHTML =
     m + ":" + s;
@@ -20,7 +79,7 @@ function checkSecond(sec) {
   if (sec < 0) {sec = "59"};
   return sec;
 }
-var userguess;
+
 
 
 //select the value of the input tag once user clicked
